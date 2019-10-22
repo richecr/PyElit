d6493335e7d079ff5e3b43f51f9471ad44052311
Rich <richelton14@gmail.com>
2019-10-22 15:26:11 -0300

    Feature de utilização do gazetteer concluída: Pode ser adicionado melhorias

--- Geoparsing/Geoparsing.py
+++ Geoparsing/Geoparsing.py
@@ -1,10 +1,13 @@
-import nltk
-from nltk.stem.porter import *
-from nltk.stem import WordNetLemmatizer
-
+import os
+import csv
 import json
 import spacy
 import geocoder
+
+import nltk
+from nltk.stem.porter import *
+from nltk.stem import WordNetLemmatizer
+from gensim.test.utils import datapath
 
 import truecase
 from googletrans import Translator
@@ -17,16 +20,21 @@ class Geoparsing:
         self.nlp.Defaults.stop_words |= {"vamos", "olha", "pois", "tudo", "coisa", "toda", "tava", "pessoal", "dessa", "resolvido", "aqui", "gente", "tá", "né", "calendário", "jpb", "agora", "voltar", "lá", "hoje", "aí", "ainda", "então", "vai", "porque", "moradores", "fazer", "prefeitura", "todo", "vamos", "problema", "fica", "ver", "tô"}
         self.stop_words_spacy = self.nlp.Defaults.stop_words
         self.residential = {}
-        self.gazetteer_ln = csv.DictReader(open("./processamento/gazetteer/gazetteer_ln.csv", "r", encoding='utf-8'))
-        self.gazetteer_pt = csv.DictReader(open("./processamento/gazetteer/gazetteer_pt.csv", "r", encoding='utf-8'))
+        self.gazetteer = {}
+        ROOT = os.path.abspath(os.path.dirname(__file__))
+        fname = ROOT + "/gazetteer/processados"
+        self.gazetteer_ln = csv.DictReader(open(fname + "/gazetteer_ln.csv", "r", encoding='utf-8'))
+        self.gazetteer_pt = csv.DictReader(open(fname + "/gazetteer_pt.csv", "r", encoding='utf-8'))
+        # self.gazetteer.update(self.pre_process(gazetteer_pt))
         self.pre_process(self.gazetteer_ln)
-    
+        self.pre_process(self.gazetteer_pt)
+
     def pre_process(self, gazetteer):
         for row in gazetteer:
             if row['fclass'] == "residential":
-                self.residencial[row['name'].lower()] = row['coordenates']
+                self.residential[row['name'].lower()] = row['coordenates']
             else:
-                self.gazetteer_ln[row['name'].lower()] = row['coordenates']
+                self.gazetteer[row['name'].lower()] = row['coordenates']
 
     def remove_stop_words(self, text):
         saida = ""
@@ -75,7 +83,6 @@ class Geoparsing:
             for ed in ends_corretos:
                 if (ed['confidence'] > end_final['confidence']):
                     end_final = ed
-            print(end_final)
             end_ = sorted(ends_corretos, key=lambda end: end['confidence'])
             return (True, end_[0:limit])
         else:
@@ -93,21 +100,50 @@ class Geoparsing:
         # Pode ser feito uma lógica para capturar os melhores endereços.
         return addresses_residentials, adresses_geral
 
-    def filterAddressCGText(text):
+    def choose_best_addresses(self, addresses_residentials, addresses_geral):
+        # TODO: Implementar algoritmos que escolham os melhores endereços
+        # Ex 1: Filtrar por endereços que estejam em um determinado bairro
+        # que também esteja entre esses endereços.
+        # Ex 2: Olhar qual endereço mais se repete no texto.
+        # Ex 3: Aplicar os dois algoritmos acima. E etc.
+        result = {}
+        result.update(addresses_residentials)
+        result.update(addresses_geral)
+        return result
+
+    def filterAddressCGText(self, text):
         addresses_residentials = {}
-        adresses_geral = {}
-        for address in self.residential:
-            if re.search("\\b" + address + "\\b", text):
-                # TODO
-                pass
+        addresses_geral = {}
+        for address in self.residential.keys():
+            address_aux = address.split()
+            if address_aux[0] == "rua":
+                address_aux = address_aux[1:]
+            if len(address_aux) > 1:
+                if re.search("\\b" + address + "\\b", text):
+                    addresses_residentials[address] = self.residential[address]
         
-        # TODO
-        return 0
+        for address in self.gazetteer.keys():
+            address_aux = address.split()
+            if address_aux[0] == "rua":
+                address_aux = address_aux[1:]
+            if len(address_aux) > 1:
+                if re.search("\\b" + address + "\\b", text):
+                    addresses_geral[address] = self.gazetteer[address]
+
+        result = self.choose_best_addresses(addresses_residentials, addresses_geral)
+        return result
 
     def geoparsing(self, text, case_correct=None, limit=5, gazettteer_cg=True):
         if (case_correct):
             if gazettteer_cg:
-                pass
+                result = self.filterAddressCGText(text.lower())
+                # TODO: Usar a biblioteca do GeoCoder para que por meio
+                # das coordenadas, seja retornado um objeto representando o
+                # endereço.
+                if result:
+                    return result
+                else:
+                    raise Exception("não foi possível realizar o geoparsing do texto")        
             else:
                 doc = self.nlp(text)
                 ents_loc = [entity for entity in doc.ents if entity.label_ == "LOC" or entity.label_ == "GPE"]
@@ -115,10 +151,10 @@ class Geoparsing:
                 result = self.verfica(address_found, limit)
                 # result[0] = self.filterAddressCG(address_found)
 
-            if (result[0]):
-                return result[1]
-            else:
-                raise Exception("Não foi possivel realizar o geoparsing do texto")
+                if (result[0]):
+                    return result[1]
+                else:
+                    raise Exception("Não foi possivel realizar o geoparsing do texto")
         else:
             text_en = self.translator.translate(text, src="pt", dest="en")
             text_en = text_en.text
@@ -139,10 +175,11 @@ class Geoparsing:
                 raise Exception("Não foi possivel realizar o geoparsing do texto")
 
 g = Geoparsing()
-r = g.geoparsing(text="quem está de volta agora com o calendário JPB hoje tem Missão Nova para nossa equipe uma praça abandonada no Ernani Sátiro aqui na capital Na verdade eu nem sei se a gente pode chamar de Praça sim viu Plínio Almeida foi até lá para conferir de um lado a Rua Abílio Paiva desse outro aqui a Auta de Luna Freire no bairro Ernani Sátiro E no meio tem algo que um dia já pode ser chamado de Praça mas do jeito que tá hoje com tudo quebrado esquecido abandonado mato lixo e sem condições nenhuma de uso Se tem uma coisa que isso aqui não é mais é uma praça os moradores dizem que faz mais ou menos uns 10 anos da última vez que alguém chegou aqui para fazer algum tipo de conserto manutenção nessa praça né Ficar Com certeza isso aqui assim eu eu eu tenho 41 anos aqui como ele tem como você já tá vendo aqui que as pessoas lhe darem a gente precisa de uma praça a gente precisa de um lazer a gente tem criança a gente precisa disso as crianças já se furaram aqui as crianças já pegaram o germe de cachorro aqui ali ó ferro enfiado para cavalo para animal aqui só serve para isso que não tem condições de andar eu mesmo vou fazer uma caminhada de Zezinho saiu daqui na rua aí rodeio e venho Quando estivesse pronto eu sou uma pracinha aqui tá certo é pequeno mas a gente aumentar o número de volta né Não só sou eu sou muito idosa que tem aqui temos a prefeitura ela tem aí uma creche em frente à praça das crianças aí vem para aí que eu já vi que ia subir aí e desceu aí pode se acidentar aí tudo rasgado tudo quebrado brincando do jeito que tá aí no jeito que tá aí criança vem amanhã à tarde para pegar porque tá vendo a hora que acidentado que a gente não pagamos também no nosso IPTU né não pagamos temos direito como colocar o outro cidadão exatamente nesse local aqui que tá cheio de mato tem Metralha também tem vários buracos era uma quadra não é isso Léo como era que antes antes de uma quadra de areia Aqui é onde a comunidade se divertir né Tem várias crianças aqui no bairro e a gente joga futebol a gente praticava vôlei não é isso para comunidade isso é muito importante queria que ele viesse reformar fazer uma quadra não colocaria novamente mas fazer uma quadra fazer um espaço que o pessoal fazer caminhada colocar ali os aparelhos para prática também de musculação isso que nós precisamos a gente que é o mesmo direito que as outros bairros tem que as outras praças tem dona Eronildes é uma das pessoas que mora aqui pertinho da praça e que tem muita vontade de fazer caminhada né mas nessas condições olha ali buraco não dá não tem não tem condições porque", case_correct=True)
+r = g.geoparsing(text="quem está de volta agora com o calendário JPB hoje tem Missão Nova para nossa equipe uma praça abandonada no Ernani Sátyro aqui na capital Na verdade eu nem sei se a gente pode chamar de Praça sim viu Plínio Almeida foi até lá para conferir de um lado a Rua Abílio Paiva desse outro aqui a Auta de Luna Freire no bairro Ernani Sátyro E no meio tem algo que um dia já pode ser chamado de Praça mas do jeito que tá hoje com tudo quebrado esquecido abandonado mato lixo e sem condições nenhuma de uso Se tem uma coisa que isso aqui não é mais é uma praça os moradores dizem que faz mais ou menos uns 10 anos da última vez que alguém chegou aqui para fazer algum tipo de conserto manutenção nessa praça né Ficar Com certeza isso aqui assim eu eu eu tenho 41 anos aqui como ele tem como você já tá vendo aqui que as pessoas lhe darem a gente precisa de uma praça a gente precisa de um lazer a gente tem criança a gente precisa disso as crianças já se furaram aqui as crianças já pegaram o germe de cachorro aqui ali ó ferro enfiado para cavalo para animal aqui só serve para isso que não tem condições de andar eu mesmo vou fazer uma caminhada de Zezinho saiu daqui na rua aí rodeio e venho Quando estivesse pronto eu sou uma pracinha aqui tá certo é pequeno mas a gente aumentar o número de volta né Não só sou eu sou muito idosa que tem aqui temos a prefeitura ela tem aí uma creche em frente à praça das crianças aí vem para aí que eu já vi que ia subir aí e desceu aí pode se acidentar aí tudo rasgado tudo quebrado brincando do jeito que tá aí no jeito que tá aí criança vem amanhã à tarde para pegar porque tá vendo a hora que acidentado que a gente não pagamos também no nosso IPTU né não pagamos temos direito como colocar o outro cidadão exatamente nesse local aqui que tá cheio de mato tem Metralha também tem vários buracos era uma quadra não é isso Léo como era que antes antes de uma quadra de areia Aqui é onde a comunidade se divertir né Tem várias crianças aqui no bairro e a gente joga futebol a gente praticava vôlei não é isso para comunidade isso é muito importante queria que ele viesse reformar fazer uma quadra não colocaria novamente mas fazer uma quadra fazer um espaço que o pessoal fazer caminhada colocar ali os aparelhos para prática também de musculação isso que nós precisamos a gente que é o mesmo direito que as outros bairros tem que as outras praças tem dona Eronildes é uma das pessoas que mora aqui pertinho da praça e que tem muita vontade de fazer caminhada né mas nessas condições olha ali buraco não dá não tem não tem condições porque", case_correct=True)
+print(r)
 
-for e in r:
-    print(e['address'])
-    print(e['confidence'])
-    print("----------------------")
+# for e in r:
+#     print(e['address'])
+#     print(e['confidence'])
+#     print("----------------------")
 # print(r)
