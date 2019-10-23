1f4f73b14716575e62ae37db2e601c72b2b40088
Rich <richelton14@gmail.com>
2019-10-23 09:36:02 -0300

    Adicionando testes para verificar a acurácia do Geoparsing com Gazetteer

--- Geoparsing/Geoparsing.py
+++ Geoparsing/Geoparsing.py
@@ -175,8 +175,30 @@ class Geoparsing:
                 raise Exception("Não foi possivel realizar o geoparsing do texto")
 
 g = Geoparsing()
-r = g.geoparsing(text="quem está de volta agora com o calendário JPB hoje tem Missão Nova para nossa equipe uma praça abandonada no Ernani Sátyro aqui na capital Na verdade eu nem sei se a gente pode chamar de Praça sim viu Plínio Almeida foi até lá para conferir de um lado a Rua Abílio Paiva desse outro aqui a Auta de Luna Freire no bairro Ernani Sátyro E no meio tem algo que um dia já pode ser chamado de Praça mas do jeito que tá hoje com tudo quebrado esquecido abandonado mato lixo e sem condições nenhuma de uso Se tem uma coisa que isso aqui não é mais é uma praça os moradores dizem que faz mais ou menos uns 10 anos da última vez que alguém chegou aqui para fazer algum tipo de conserto manutenção nessa praça né Ficar Com certeza isso aqui assim eu eu eu tenho 41 anos aqui como ele tem como você já tá vendo aqui que as pessoas lhe darem a gente precisa de uma praça a gente precisa de um lazer a gente tem criança a gente precisa disso as crianças já se furaram aqui as crianças já pegaram o germe de cachorro aqui ali ó ferro enfiado para cavalo para animal aqui só serve para isso que não tem condições de andar eu mesmo vou fazer uma caminhada de Zezinho saiu daqui na rua aí rodeio e venho Quando estivesse pronto eu sou uma pracinha aqui tá certo é pequeno mas a gente aumentar o número de volta né Não só sou eu sou muito idosa que tem aqui temos a prefeitura ela tem aí uma creche em frente à praça das crianças aí vem para aí que eu já vi que ia subir aí e desceu aí pode se acidentar aí tudo rasgado tudo quebrado brincando do jeito que tá aí no jeito que tá aí criança vem amanhã à tarde para pegar porque tá vendo a hora que acidentado que a gente não pagamos também no nosso IPTU né não pagamos temos direito como colocar o outro cidadão exatamente nesse local aqui que tá cheio de mato tem Metralha também tem vários buracos era uma quadra não é isso Léo como era que antes antes de uma quadra de areia Aqui é onde a comunidade se divertir né Tem várias crianças aqui no bairro e a gente joga futebol a gente praticava vôlei não é isso para comunidade isso é muito importante queria que ele viesse reformar fazer uma quadra não colocaria novamente mas fazer uma quadra fazer um espaço que o pessoal fazer caminhada colocar ali os aparelhos para prática também de musculação isso que nós precisamos a gente que é o mesmo direito que as outros bairros tem que as outras praças tem dona Eronildes é uma das pessoas que mora aqui pertinho da praça e que tem muita vontade de fazer caminhada né mas nessas condições olha ali buraco não dá não tem não tem condições porque", case_correct=True)
-print(r)
+
+textos_limpos = []
+titulos = []
+arq = csv.DictReader(open("../dados/textos_videos.csv", "r", encoding='utf-8'))
+
+sucess = 0
+fail = 0
+total = 0
+for p in arq:
+    if (total == 100):
+        break
+    try:
+        r = g.geoparsing(text=p['texto'], case_correct=True)
+        # print(r)
+        total += 1
+        sucess += 1
+    except Exception as error:
+        total += 1
+        fail += 1
+        # print(error)
+
+print("Total: ", total)
+print("Sucess: ", sucess)
+print("Fail: ", fail)
 
 # for e in r:
 #     print(e['address'])
